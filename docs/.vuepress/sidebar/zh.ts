4286a6a1517e9e9ce6dafa13b1322460d1192b2c
ruguoaaa <ruguoaaa@163.com>
2023-08-28 15:20:23 +0800

    disable search

--- docs/.vuepress/sidebar/zh.ts
+++ docs/.vuepress/sidebar/zh.ts
@@ -1,16 +1,7 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "https://ruguoaaa.github.io/xirang/诗藏/": "structure",
-  "https://ruguoaaa.github.io/xirang1/医藏/": "structure",
-  "https://ruguoaaa.github.io/xirang2/易藏/": "structure",
-  "https://ruguoaaa.github.io/xirang3/史藏/": "structure",
-  "https://ruguoaaa.github.io/xirang4/子藏/": "structure",
-  "https://ruguoaaa.github.io/xirang5/集藏/": "structure",
-  "https://ruguoaaa.github.io/xirang6/艺藏/": "structure",
-  "https://ruguoaaa.github.io/xirang7/佛藏/": "structure",
-  "https://ruguoaaa.github.io/xirang8/儒藏/": "structure",
-  "https://ruguoaaa.github.io/xirang9/道藏/": "structure",
+  "/诗藏/": "structure",
 
   "/": ["", "donate"],
 });
